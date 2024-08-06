6c22be0e5154ab852afa7870eb374c986e87842b
Beapoe <2713906889@qq.com>
2024-08-06 11:38:15 +0800

    Hello World

---
+++ app/src/main/java/team/CY/calculator/main.java
@@ -0,0 +1,14 @@
+package team.CY.calculator;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class main extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
